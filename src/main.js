c2748591c93af2645d0cc85a1dfaff282bdd6362
Robbert M <=>
2022-02-19 09:17:16 +0100

    Force comment loading, don't show generic when other timestamps have been found

--- src/main.js
+++ src/main.js
@@ -1,32 +1,77 @@
-const TIME_RE = /([0-9]+:[0-9]+)/g;
+const TIME_RE = /([0-9]{1,2}:[0-9]{2})/
+const COMMENT_LOAD_TIMEOUT = 1000
+
+const api = new class Api {
+  #MAX_SLEEP_INTERVAL = 10000
+
+  async getComments() {
+    await this.#triggerCommentLoad()
+    await this.#waitForComments()
+
+    return [...this
+      .#getCommentContainer()
+      .querySelectorAll('.c-comment__body .c-text')
+    ].map(it => it.innerText)
+  }
+
+  async #triggerCommentLoad() {
+    window.scrollBy(0, window.innerHeight)
+    await this.#sleep(COMMENT_LOAD_TIMEOUT)
+    window.scrollBy(0, 0 - window.innerHeight)
+  }
+
+  async #waitForComments() {
+    return new Promise(async (resolve) => {
+      let sleepInterval = 1000
+      while (this.#getCommentContainer() == null) {
+        await this.#sleep(sleepInterval)
+        sleepInterval = sleepInterval >= this.#MAX_SLEEP_INTERVAL
+          ? sleepInterval
+          : sleepInterval * 1.5
+      }
+
+      resolve()
+    })
+  }
+
+  #getCommentContainer() {
+    return document.querySelector('.erc-comments')
+  }
+
+  async #sleep(millis) {
+    return new Promise(res => setTimeout(res, millis))
+  }
+}()
 
 window.addEventListener('load', async () => {
   document.domain = 'crunchyroll.com'
 
-  window.addEventListener('message', event => {
+  window.addEventListener('message', async event => {
     try {
-      const { origin } = event
-      const { type } = (event.data || {})
+      const {origin} = event
+      const {type} = (event.data || {})
 
-      if (
-        origin !== 'https://static.crunchyroll.com' ||
-        type == null || type !== 'video-loaded'
-      ) return
+      if (origin !== 'https://static.crunchyroll.com' || type == null || type !== 'video-loaded') return
 
-      const comments = [...document
-        .querySelector('.erc-comments')
-        .querySelectorAll('.c-text')
-      ].map(it => it.innerText)
-
+      const comments = await api.getComments()
       const possibleSkips = comments
         .map(it => it.match(TIME_RE))
         .filter(it => it != null)
-        .map(it => it[0]);
+        .map(it => {
+          const [minutes, seconds] = it[0].split(':').map(n => parseInt(n, 10))
 
+          return ({
+            time: 60 * minutes + seconds,
+            timeString: `${minutes}:${seconds.toString().padStart(2, '0')}`,
+            comment: it.input
+          });
+        });
+
+      document.domain = 'crunchyroll.com'
       window.postMessage({
-        type: 'announce-skip-times',
-        data: possibleSkips
+        type: 'announce-skip-times', data: possibleSkips
       }, '*')
-    } catch(_) {}
+    } catch (_) {
+    }
   })
 })
--- src/player.js
+++ src/player.js
@@ -1,16 +1,42 @@
 /**
+ * @typedef {'recap'|'titlecard'|'generic'} CommentAttributesType
+ */
+
+const TRY_SKIP_GENERIC = true
+
+const TIME_RE = /[0-9]{1,2}:[0-9]{2}/
+const RECAP_RE = /recap|episode/i
+const TITLECARD_RE = /TC|title/i
+
+const COMMENT_SPLIT_RE = new RegExp(
+  [
+    `(${RECAP_RE.source}).*?(?<recap>${TIME_RE.source})`,
+    `(${TITLECARD_RE.source}).*?(?<titlecard>${TIME_RE.source})`
+  ].join('|'),
+  'gi'
+)
+
+/**
  * Threshold (in seconds) to filter out almost-the-same announced skip times
  */
 const TIME_DELTA = 5
+/**
+ * Time in ms to wait for the main window to load. The player is faster sometimes
+ */
+const MAIN_WAIT_TIMEOUT = 1000
+/**
+ * Time in ms to wait for the buttons to disappear if they are not clicked
+ */
+const HIDE_BUTTON_TIMEOUT = 10000
 
-const BUTTON_CONTAINER_STYLE = `
+const /**CSSStyleDeclaration*/ BUTTON_CONTAINER_STYLE = `
   position: fixed;
   right: 5px;
   bottom: 80px;
   z-index: 999999;
 `
 
-const BUTTON_STYLE = `
+const /**CSSStyleDeclaration*/ BUTTON_STYLE = `
   color: white;
   background-color: rgba(0, 0, 0, 0.25);
   border: 1px solid white;
@@ -20,9 +46,95 @@ const BUTTON_STYLE = `
   cursor: pointer;
 `
 
-window.addEventListener('load', async () => {
-  document.domain = 'crunchyroll.com'
+const calculateTime = (timeString) => {
+  const [minutes, seconds] = timeString.match(TIME_RE)[0].split(':').map(n => parseInt(n, 10))
+  return {
+    time: 60 * minutes + seconds,
+    timeString: `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }
+}
 
+class CommentAttributes {
+  time
+  timeString
+  type
+
+  skipOthers
+  showMe
+  skipText
+
+  constructor(
+    time,
+    timeString,
+    type
+  ) {
+    this.time = time
+    this.timeString = timeString
+    this.type = type
+
+    this.skipOthers = type !== 'generic'
+    this.showMe = this.skipOthers
+
+    this.skipText = this.#getSkipText()
+  }
+
+  /**
+   * @param {string} comment
+   * @param {number} time
+   * @param {string} timeString
+   * @returns {CommentAttributes[]}
+   */
+  static create(comment, time, timeString) {
+    const recap = comment.match(RECAP_RE)
+    const titlecard = comment.match(TITLECARD_RE)
+
+    if (recap != null && titlecard != null) {
+      const split = {
+        recapTime: time,
+        recapTimeString: timeString,
+        titlecardTime: time,
+        titlecardTimeString: timeString
+      }
+
+      comment.match(COMMENT_SPLIT_RE).forEach((partial, index) => {
+        const ts = partial.match(TIME_RE)[0]
+        if(partial.match(RECAP_RE) != null) {
+          const {time: recapTime, timeString: recapTimeString} = calculateTime(ts)
+          split.recapTime = recapTime
+          split.recapTimeString = recapTimeString
+        }
+        if(partial.match(TITLECARD_RE)) {
+          const {time: titlecardTime, timeString: titlecardTimeString} = calculateTime(ts)
+          split.titlecardTime = titlecardTime
+          split.titlecardTimeString = titlecardTimeString
+        }
+      })
+
+      return [
+        new CommentAttributes(split.recapTime, split.recapTimeString, 'recap'),
+        new CommentAttributes(split.titlecardTime, split.titlecardTimeString, 'titlecard'),
+      ]
+    } else {
+      const type = recap != null ? 'recap' : titlecard != null ? 'titlecard' : 'generic'
+      return [
+        new CommentAttributes(time, timeString, type)
+      ]
+    }
+  }
+
+  #getSkipText() {
+    switch (this.type) {
+      case 'titlecard':
+        return `Skip to title (${this.timeString})`
+      case 'recap':
+        return `Skip recap (${this.timeString})`
+      default:
+        return `Skip to ${this.timeString}`
+    }
+  }
+}
+
+window.addEventListener('load', async () => {
   const $video = document.querySelector('video')
   const $container = document.querySelector('[data-testid="vilos-root_container"]')
 
@@ -31,6 +143,10 @@ window.addEventListener('load', async () => {
     window.top.postMessage({type: 'video-loaded'}, '*')
   })
 
+  // wait a bit for the main content to load
+  await new Promise(res => setTimeout(res, MAIN_WAIT_TIMEOUT))
+
+  document.domain = 'crunchyroll.com'
   window.top.addEventListener('message', event => {
     try {
       const {origin} = event
@@ -45,22 +161,25 @@ window.addEventListener('load', async () => {
       const $buttonContainer = document.createElement('div')
       $buttonContainer.style = BUTTON_CONTAINER_STYLE
       $container.appendChild($buttonContainer)
-
       data
-        .map(it => {
-          const [minutes, seconds] = it.split(':').map(n => parseInt(n, 10))
-          return {text: `Skip to: ${it}`, time: 60 * minutes + seconds}
+        .flatMap(({time, timeString, comment}) => CommentAttributes.create(comment, time, timeString))
+        .filter(({showMe}, _, self) => {
+          if (TRY_SKIP_GENERIC && self.some(({skipOthers}) => skipOthers)) {
+            return showMe === true
+          }
+
+          return true
         })
         .sort((a, b) => a.time - b.time)
-        .filter((value, index, self) =>
+        .filter(({time}, index, self) =>
           self.findIndex(it =>
-            it.time >= value.time - TIME_DELTA &&
-            it.time <= value.time + TIME_DELTA
+            it.time >= time - TIME_DELTA &&
+            it.time <= time + TIME_DELTA
           ) === index
         )
-        .forEach(({text, time}) => {
+        .forEach(({skipText, time}) => {
           const $button = document.createElement('button')
-          $button.innerHTML = text
+          $button.innerHTML = skipText
           $button.style = BUTTON_STYLE
 
           $button.onclick = () => {
@@ -75,7 +194,7 @@ window.addEventListener('load', async () => {
           $container.removeChild($buttonContainer)
         } catch (_) {
         }
-      }, 5000)
+      }, HIDE_BUTTON_TIMEOUT)
     } catch (_) {
     }
   }, false)
